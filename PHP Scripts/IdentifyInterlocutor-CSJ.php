b8d924d0d8cca4219b89028fff2f7412acd0a130
Michael Bonney <bonney12@users.noreply.github.com>
2022-08-27 02:09:16 +0100

    Refactored IdentifyInterlocutor-CSJ to insert both Speaker and Interlocutor data

--- PHP Scripts/IdentifyInterlocutor-CSJ.php
+++ PHP Scripts/IdentifyInterlocutor-CSJ.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Reformat Database Script - CSJ
+ * IdentifyInterlocutor Script - CSJ
  *
  * This script modifies the CSJ database in order to fill columns that will identify
- * the speaker of a given utterance. The script will evaluate the TalkID to ascertain
- * if the channel of the utterance is L or R. Then, it will use the Talk_* columns to
- * set the correct data in the Speaker_ columns.
+ * the speaker and listener of a given utterance. The script will evaluate the TalkID
+ * to ascertain if the channel of the utterance is L or R. Then, it will use the Talk_*
+ * columns to set the correct data in the Speaker_ and Interlocutor_ columns.
  *
  * @author Michael Bonney <michael@mbonney.uk>
  * @copyright cc-by-sa
@@ -22,13 +22,12 @@ $database = '';        // <-- Put your database name here
 $username = '';        // <-- Put your database username here
 $password = '';        // <-- Put your database password here
 $charset  = 'utf8mb4';
-
 //
 // WARNING: Do not edit below this line unless familiar with coding.
 //
 
 echo "//\n";
-echo "// Executing ReformatDatabase - CSJ\n";
+echo "// Executing IdentifyInterlocutor - CSJ\n";
 echo "//\n\n";
 
 // Create DSN
@@ -73,43 +72,41 @@ foreach($results AS $result) {
   // R always interviewee - cf. p114 of kokken124 - 日本語話し言葉コーパスの構築法)
   $channel = substr($result['ClauseID'], -1);
 
-  // If channel is 'L'...
+  $speakers = [
+    'interviewer' => [
+      'sex' => $result['Talk_Interviewer_Sex'],
+      'age' => $result['Talk_Interviewer_AgeRange'],
+      'birthplace' => $result['Talk_Interviewer_BirthPlace']
+    ],
+
+    'speaker' => [
+      'sex' => $result['Talk_Speaker_Sex'],
+      'age' => $result['Talk_Speaker_AgeRange'],
+      'birthplace' => $result['Talk_Speaker_BirthPlace']
+    ]
+  ];
+
   if($channel == 'L') {
-    // Report to CLI
-    echo "[INFO] [UPDATING] [L] Setting clause " . $result['ClauseID'] . " from talk " . $result['TalkID'] . " (" . $result['Talk_Interviewer_Sex'] . ", " . $result['Talk_Interviewer_AgeRange'] . ", " . $result['Talk_Interviewer_BirthPlace'] . ")\n";
-
-    // Person who produced utterance is Interviewer, so prepare query and execute.
-    $updateQuery = $pdo->prepare('UPDATE `CSJ Results` SET `Speaker_Sex` = ?, `Speaker_AgeRange` = ?, `Speaker_BirthPlace` = ? WHERE `TalkID` = ? AND `ClauseID` = ? AND `StartTime` = ? AND `EndTime` = ?');
-    $updateQuery->execute([
-      $result['Talk_Interviewer_Sex'],
-      $result['Talk_Interviewer_AgeRange'],
-      $result['Talk_Interviewer_BirthPlace'],
-      $result['TalkID'],
-      $result['ClauseID'],
-      $result['StartTime'],
-      $result['EndTime']
-    ]);
-    // Skip rest of the code here as there is no further setting at this point -
-    // continue will move to the next iteration
-    continue;
+    $speakerData = $speakers['interviewer'];
+    $interlocutorData = $speakers['speaker'];
+  } elseif($channel == 'R') {
+    $speakerData = $speakers['speaker'];
+    $interlocutorData = $speakers['interviewer'];
   }
 
-  if($channel == 'R') {
-    // Report to CLI
-    echo "[INFO] [UPDATING] [R] Setting clause " . $result['ClauseID'] . " from talk " . $result['TalkID'] . " (" . $result['Talk_Speaker_Sex'] . ", " . $result['Talk_Speaker_AgeRange'] . ", " . $result['Talk_Speaker_BirthPlace'] . ")\n";
+  echo "[INFO] [UPDATING] [L] Setting clause " . $result['ClauseID'] . " from talk " . $result['TalkID'] . " (Speaker data is " . $speakerData['sex'] . ", " . $speakerData['age'] . ", " . $speakerData['birthplace'] . ")\n";
 
-    // Person who produced utterance is Speaker, so prepare query and execute.
-    $updateQuery = $pdo->prepare('UPDATE `CSJ Results` SET `Speaker_Sex` = ?, `Speaker_AgeRange` = ?, `Speaker_BirthPlace` = ? WHERE `TalkID` = ? AND `ClauseID` = ? AND `StartTime` = ? AND `EndTime` = ?');
-    $updateQuery->execute([
-      $result['Talk_Speaker_Sex'],
-      $result['Talk_Speaker_AgeRange'],
-      $result['Talk_Speaker_BirthPlace'],
-      $result['TalkID'],
-      $result['ClauseID'],
-      $result['StartTime'],
-      $result['EndTime']
-    ]);
-  }
+  // Person who produced utterance is Interviewer, so prepare query and execute.
+  $updateQuery = $pdo->prepare('UPDATE `CSJ Results` SET `Speaker_Sex` = ?, `Speaker_AgeRange` = ?, `Speaker_BirthPlace` = ?, `Interlocutor_Sex` = ?, `Interlocutor_AgeRange` = ?, `Interlocutor_BirthPlace` = ? WHERE `ID` = ?');
+  $updateQuery->execute([
+    $speakerData['sex'],
+    $speakerData['age'],
+    $speakerData['birthplace'],
+    $interlocutorData['sex'],
+    $interlocutorData['age'],
+    $interlocutorData['birthplace'],
+    $result['ID']
+  ]);
 }
 
 echo "[INFO] Completed successfully";
